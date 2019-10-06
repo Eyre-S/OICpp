9c52d00c0b766a25f71d6e26775bd807b8046855
Eyre_S <SteveW2004@outlook.com>
2019-10-06 08:36:14 +0800

    B：完成INPUT部分

--- src/noiac/NC-CT-S01 模拟赛第六场/- B string/string.cpp
+++ src/noiac/NC-CT-S01 模拟赛第六场/- B string/string.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+string common_str;
+int str_len;
+int ask_count = 0;
+
 int main () {
+	
+	cin >> ask_count;
+	for (register int timey = 0; timey < ask_count; timey) {
+		cin >> str_len;
+		getchar(); cin >> common_str;
+	}
 	
 	return 0;
 	
