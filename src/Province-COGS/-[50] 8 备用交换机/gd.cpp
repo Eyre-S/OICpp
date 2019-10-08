1e6e1465aa191e9f089da9504ee7e2f22f0cbc77
Eyre_S <SteveW2004@outlook.com>
2019-10-08 16:40:18 +0800

    8：更新代码风格
    添加头文件
    更新缩进
    修复一个测试输出问题

--- src/Province-COGS/-[50] 8 备用交换机/gd.cpp
+++ src/Province-COGS/-[50] 8 备用交换机/gd.cpp
@@ -1,51 +1,68 @@
+/**
+ *     __   _____       __                              __
+ *    / /  / ___/__  __/ /______ _____  __  ______     / /
+ *   / /   \__ \/ / / / //_/ __ `/_  / / / / / __ \   / /
+ *  /_/   ___/ / /_/ / ,< / /_/ / / /_/ /_/ / /_/ /  /_/
+ * (_)   /____/\__,_/_/|_|\__,_/ /___/\__, /\____/  (_)
+ *                                   /____/
+ *
+ *
+ * COGS - 备用交换机（求割点裸题）
+ * 作者：Cookie Sukazyo Eyre
+ *
+ * 使用算法：tarjan
+ * 存图类型：数组型邻接表
+ *
+ */
+
 #include <iostream>
 #include <cstdio>
 #include <memory.h>
- 
+
 using namespace std;
- 
+
 // 城市数
 int citiesNum = 0;
- 
+
 int indexs = 1;
- 
+
 // 城市是否重要
 bool cut[100]; // 是否为切点
- 
+
 // 图储存
 int line[100]; // 节点的最后一条线段
 int nextl[10000]; // 线段的邻线段
 int to[10000]; // 线段目标节点
- 
+
 // 搜索相关
 int dfn[100] = {0};// 搜索ID
 int tag[100] = {0}; // 强连通标签
 bool alive[10000]; // 线段是否可用（未访问过）
- 
+
 int rootSingle = -1;
- 
+
 // 函数声明
 int dfs (int);
- 
+
 int main () {
- 
+	
 	// 重定向IO
 	freopen("gd.in", "r", stdin);
 	freopen("gd.out", "w", stdout);
- 
+	
 	cin >> citiesNum;
- 
+	
 	// 元素初始化 -1为空
 	memset(line, -1, sizeof(line));
 	memset(dfn, 0, sizeof(dfn));
 	memset(to, -1, sizeof(to));
 	memset(nextl, -1, sizeof(nextl));
- 
+	
 	// 构图
 	int ca, cb;
 	for (int i = 0; !cin.eof(); i += 2) {
 		// 双（无）向边存储存图
- 
+		
 		cin >> ca >> cb;
 		// cout << "Get " << ca << " " << cb << endl;
 		ca--;cb--;
@@ -64,19 +81,19 @@ int main () {
 			rootSingle++;
 		}
 	}
- 
+	
 	// for (int i = 0; i < 10000; i++) {
 	// 	cout << i << "\t" << to[i] << "\t" << nextl[i] << endl;
 	// }
- 
+	
 	dfs(0);
- 
+	
 	// for (int i = 0; i < citiesNum; i++) {
 	// 	cout << i+1 << "\t" << dfn[i] << "\t" << tag[i] << endl;
 	// }
- 
+	
 	// cout << rootSingle << " ";
- 
+	
 	int counter = 0;
 	string listing;
 	for (int i = 0; i < citiesNum; i++) {
@@ -93,17 +110,17 @@ int main () {
 	
 	return 0;
 }
- 
+
 // tarjan搜索主循环
 int dfs (int node) {
- 
+	
 	// cout << "Enter " << node + 1 << endl;
 	if(dfn[node] == 0) {
- 
+		
 		dfn[node] = indexs;
 		tag[node] = indexs;
 		indexs++;
- 
+		
 		// cout << " - entered" << " indexd " << dfn[node] << endl;
 		int l;
 		while ((l = line[node]) != -1) {
@@ -123,7 +140,7 @@ int dfs (int node) {
 						cut[node] = true;
 					} else {
 						if (rootSingle > 0) {
-							if (childTag > dfn[0])
+							if (childTag > dfn[node])
 							cut[0] = true;
 						}
 					}
@@ -131,9 +148,9 @@ int dfs (int node) {
 			}
 		}
 	}
- 
-	// cout << "left " << node + 1 << "return" << tag[node] << endl;
- 
+	
+	// cout << "left " << node + 1 << " return " << tag[node] << endl;
+	
 	return tag[node];
- 
+
 }
