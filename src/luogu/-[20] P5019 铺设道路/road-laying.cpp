132bce23e1fd529686f16be2531744d49d65a89d
Eyre_S <SteveW2004@outlook.com>
2019-10-29 22:04:29 +0800

    sub1 添加 testdata1

--- src/luogu/-[20] P5019 铺设道路/road-laying.cpp
+++ src/luogu/-[20] P5019 铺设道路/road-laying.cpp
@@ -32,7 +32,7 @@ int main () {
 	register int total_zero = 0; // 已经平坦的数量
 	while (total_zero != road_len) {
 		// 对每个路段检查
-		for (register int i = 0, start = 0, min = 10001, now_zero = 0; i < road_len; i++) {
+		for (register int i = 0, start = 0, min = 34748, now_zero = 0; i < road_len; i++) {
 			// cout << min << endl;
 			// 如果存在工作段
 			if (road[i] == 0 /* 部分路段 */|| i == road_len - 1 /* 全部路段 */) {
@@ -42,11 +42,11 @@ int main () {
 						// 找到最小值
 						min = road[i];
 						now_zero = 1;
-						// cout << "Resize zero" << endl;
+						cout << "Resize zero" << endl;
 					}
 					i++;
 				}
-				// cout << "Find workspace from " << start << " to " << i - 1 << endl;
+				cout << "Find workspace from " << start << " to " << i - 1 << endl;
 				// 循环铺平路段
 				for (register int p = start; p < i; p++) {
 					road[p] -= min;
@@ -54,33 +54,33 @@ int main () {
 				// 记录本次工作
 				work_times += min;
 				total_zero += now_zero;
-				// cout << " - Zero add " << now_zero << " to " << total_zero << endl;
-				// cout << " - ";for (register int p = 0; p < road_len; p++) {
-				// 	cout << road[p] << ' ';
-				// }cout << endl;
-				// cout << " - Worked " << work_times << endl;
+				cout << " - Zero add " << now_zero << " to " << total_zero << endl;
+				cout << " - ";for (register int p = 0; p < road_len; p++) {
+					cout << road[p] << ' ';
+				}cout << endl;
+				cout << " - Worked " << work_times << endl;
 				while (road[i] == 0 && i < road_len) {
 					i++;
 				} start = i; i--;
 				min = 10001; now_zero = 0;
-				// cout << " - " << start << ' ' << i << endl;
+				cout << " - " << start << ' ' << i << endl;
 			} else if (road[i] < min) {
 				// 找到最小值
 				min = road[i];
 				now_zero = 1;
-				// cout << "Resize zero" << endl;
+				cout << "Resize zero" << endl;
 			} else if (road[i] == min) {
 				// 找到和最小值同样的值
 				now_zero++;
-				// cout << " - Zero++ to " << now_zero << endl;
+				cout << " - Zero++ to " << now_zero << endl;
 			}
 		}
 		// cout << total_zero << endl;
 	}
 	
-	// for (register int i = 0; i < road_len; i++) {
-	// 	cout << road[i] << ' ';
-	// }cout << endl;
+	for (register int i = 0; i < road_len; i++) {
+		cout << road[i] << ' ';
+	}cout << endl;
 	
 	cout << work_times << endl;
 	
