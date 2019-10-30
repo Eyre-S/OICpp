fafe578a02d541a6da8fb8726ae131de2213cb77
Eyre_S <SteveW2004@outlook.com>
2019-10-30 20:40:46 +0800

    修复初始0未统计 & 开头0导致的卡判断

--- src/luogu/+[60] P5019 铺设道路/road-laying.cpp
+++ src/luogu/+[60] P5019 铺设道路/road-laying.cpp
@@ -32,9 +32,28 @@ int main () {
 	}
 	
 	register int total_zero = 0; // 已经平坦的数量
+	
+	for (register int i = 0; i < road_len; i++) {
+		if (road[i] == 0) {
+			#ifdef DEBUG
+				cout << "Prefind zero at " << i << endl;
+			#endif
+			total_zero++;
+		}
+	}
+	
 	while (total_zero != road_len) {
 		// 对每个路段检查
 		for (register int i = 0, start = 0, min = 34748, now_zero = 0; i < road_len; i++) {
+			// 修复开头为0时的工作段统计错误
+			if (i == 0) {
+				while (road[i] == 0 && i < road_len) {
+					#ifdef DEBUG
+						cout << "Find zero at " << i << " and i++" << endl;
+					#endif
+					i++;
+				} start = i;
+			}
 			// cout << min << endl;
 			// 如果存在工作段
 			if (road[i] == 0 /* 部分路段 */|| i == road_len - 1 /* 全部路段 */) {
@@ -67,6 +86,7 @@ int main () {
 					}cout << endl;
 					cout << " - Worked " << work_times << endl;
 				#endif
+				// 初始化下次要用的值
 				while (road[i] == 0 && i < road_len) {
 					i++;
 				} start = i; i--;
