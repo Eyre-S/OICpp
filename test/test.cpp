2aac3e87a82aaf235e013b5d97a8dfc235f3de8b
Eyre_S <SteveW2004@outlook.com>
2019-11-02 12:03:30 +0800

    优化编译环境 在build03中添加详细的警告提示和DEBUG宏定义

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,11 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+
 using namespace std;
-int main()
-{
-int a=1;
-system("msg %username% /time:300 “请马上离开本计算机！此计算机即将爆炸！”");
-while(1)
-system("start cmd while ");
-return 0;
+
+int main () {
+	
+	cout << "Hello World" << endl;
+	
+	return 0;
+	
 }
