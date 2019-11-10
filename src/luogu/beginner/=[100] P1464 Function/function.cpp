28874b8f8be1cafe424c706e8864dc3ecd4e9e01
Eyre_S <SteveW2004@outlook.com>
2019-11-10 18:47:11 +0800

    P1464 完成记忆化查寻

--- src/luogu/beginner/=[100] P1464 Function/function.cpp
+++ src/luogu/beginner/=[100] P1464 Function/function.cpp
@@ -16,18 +16,25 @@
 
 #include <iostream>
 #include <cstdio>
+#include <memory.h>
 
 using namespace std;
 typedef long long int ll;
 // using ll = register long long int;
 
+int func_mem[20][20][20];
+
 ll func(ll , ll , ll);
 
 int main () {
 	
+	// 初始化空的记忆矩阵
+	memset(func_mem, -1, sizeof(func_mem));
+	
 	ll ina, inb, inc;
 	cin >> ina >> inb >> inc;
 	do {
+		// 循环求函数值
 		printf("w(%lld, %lld, %lld) = %lld\n", ina, inb, inc, func(ina,inb,inc));
 		cin >> ina >> inb >> inc;
 	} while (ina!=-1 || inb!=-1 || inc!=-1);
@@ -41,12 +48,14 @@ ll func (register ll pa,register ll pb,register ll pc) {
 		(pa<=0 || pb <=0 || pc<=0)?
 		1
 		:(
-			pa>20 || pb> 20 || pc>20?
+			(pa>20 || pb> 20 || pc>20)?
 			func(20, 20, 20)
 			:(
-				(pa<pb && pb<pc)?
-				(func(pa, pb, pc-1) + func(pa, pb-1, pc-1) - func(pa, pb-1, pc))
-				:(func(pa-1,pb,pc)+func(pa-1,pb-1,pc)+func(pa-1,pb,pc-1)-func(pa-1,pb-1,pc-1))
+				(func_mem[pa-1][pb-1][pc-1]==-1)?(
+					(pa<pb && pb<pc)?
+					(func_mem[pa-1][pb-1][pc-1] = func(pa, pb, pc-1) + func(pa, pb-1, pc-1) - func(pa, pb-1, pc), func_mem[pa-1][pb-1][pc-1])
+					:(func_mem[pa-1][pb-1][pc-1] = func(pa-1,pb,pc)+func(pa-1,pb-1,pc)+func(pa-1,pb,pc-1)-func(pa-1,pb-1,pc-1), func_mem[pa-1][pb-1][pc-1])
+				):(func_mem[pa-1][pb-1][pc-1])
 			)
 		)
 	);
