cbbf7868ffb7805bef195810257e23c549e9db6e
Eyre_S <SteveW2004@outlook.com>
2019-11-14 12:53:20 +0800

    P3367 完成方案

---
+++ src/main/luogu/0-0 模板/- P3367 【模板】并查集/mergecheck-set.cpp
@@ -0,0 +1,64 @@
+/**
+ *     __   _____       __                              __
+ *    / /  / ___/__  __/ /______ _____  __  ______     / /
+ *   / /   \__ \/ / / / //_/ __ `/_  / / / / / __ \   / / 
+ *  /_/   ___/ / /_/ / ,< / /_/ / / /_/ /_/ / /_/ /  /_/  
+ * (_)   /____/\__,_/_/|_|\__,_/ /___/\__, /\____/  (_)   
+ *                                   /____/               
+ * 
+ * 
+ * 洛谷 - P3367 【模板】并查集
+ * <https://www.luogu.org/problem/P3367>
+ * 
+ * 作者：Cookie Sukazyo Eyre
+ * 
+ */
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int element_count;
+int action_set;
+
+int node_father[10000];
+list<int> child[10000];
+
+void link (int, int);
+bool check (int, int);
+
+int main () {
+	
+	for (register int i = 0; i < element_count; i++) {
+		node_father[i] = i;
+	}
+	
+	cin >> element_count >> action_set;
+	
+	int act, one, another;
+	for (register int i = 0; i < action_set; i++) {
+		cin >> act >> one >> another;
+		if (act == 1) {
+			link(one, another);
+		} else {
+			cout << (check(one, another)?'Y':'N') << endl;
+		}
+	}
+	
+	return 0;
+	
+}
+
+void link (int first, int second) {
+	node_father[second] = node_father[first];
+	child[first].push_back(second);
+	for (list<int>::iterator i = child[second].begin(); i != child[second].end(); i++) {
+		node_father[*i] = node_father[first];
+	}
+	child[second].clear();
+}
+
+inline bool check (int a, int b) {
+	return (node_father[a] == node_father[b] || node_father[a] == b || a == node_father[b] ) ? true : false;
+}
